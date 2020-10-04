7af0671b1b72bdbfa8d2f9eb2b980719d45e4df8
TREXXX27 <TREXXX27@users.noreply.github.com>
2020-10-05 01:31:43 +0530

    Added definition for BinaryHeap class in BHeap.h

--- Heaps/BHeap.h
+++ Heaps/BHeap.h
@@ -1,6 +1,29 @@
 #pragma once
 
-namespace ds 
-{
+#include<iostream>
+#include<vector>
 
+namespace ds
+{
+	class BinaryHeap
+	{
+		//private data members and member functions.
+	private:
+		std::vector <int> heap;
+		int parent(int child);
+		int left(int parent);
+		int right(int parent);
+		void bubbleup(int index);
+		void bubbledown(int index);
+
+		//public data members and member functions.
+	public:
+		BinaryHeap() {}
+		int Size();
+		void Insert(int n);
+		void Poll();
+		void Delete(int n);
+		void Display();
+
+	};
 }
--- Heaps/Binary_Heap.cpp
+++ Heaps/Binary_Heap.cpp
@@ -8,27 +8,7 @@
 
 using namespace std;
 
-class BinaryHeap					
-{
-//private data members and member functions.
-private:
-	vector <int> heap;
-	int parent(int child);
-	int left(int parent);
-	int right(int parent);
-	void bubbleup(int index);
-	void bubbledown(int index);
 
-//public data members and member functions.
-public:
-	BinaryHeap() {}
-	int Size();
-	void Insert(int n);
-	void Poll();
-	void Delete(int n);
-	void Display();
-
-};
 
 // Returns size of heap.
 int BinaryHeap::Size()				

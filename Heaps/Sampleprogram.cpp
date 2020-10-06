3bfec4bb73c3d8653be21e72f46e6e0a3eb0afaa
Kushagra Kalash <kushagrakdev@gmail.com>
2020-10-07 01:32:07 +0530

    minor refactoring in sampleprogram.cpp

--- Heaps/Sampleprogram.cpp
+++ Heaps/Sampleprogram.cpp
@@ -10,56 +10,61 @@
 using namespace std;
 using namespace ds;
 
-// Main function.
 int main()
 {
-	BinaryHeap<int> obj;
+    BinaryHeap<int> obj;
 
-	while (1)
-	{
-		int c, ele;
+    while (1)
+    {
+        int c, ele;
 
-		cout << "1.Insert Element" << "\n";
-		cout << "2.TOP OF heap" << "\n";
-		cout << "3.Delete Element" << "\n";
-		cout << "4.Display Heap" << "\n";
-		cout << "5.Exit" << "\n";
-		cout << "Enter your choice: ";
+        cout << "1. Insert Element" << "\n";
+        cout << "2. Show top of heap" << "\n";
+        cout << "3. Delete Element" << "\n";
+        cout << "4. Display Heap" << "\n";
+        cout << "5. Exit" << "\n";
+        cout << "Enter your choice: ";
 
-		cin >> c;
-		try
-		{
-			switch (c)
-			{
-			case 1:
-				cout << "Enter the element to be inserted: ";
-				cin >> ele;
-				obj.Insert(ele);
-				break;
-			case 2:
-				cout << "Top element is : " << obj.Poll() << "\n";
-				break;
-			case 3:
-				cout << "Enter the element to be deleted: ";
-				cin >> ele;
-				obj.Delete(ele);
-				obj.Display();
-				break;
-			case 4:
-				obj.Display();
-				break;
-			case 5:
-				exit(0);
-			default:
-				cout << "Enter Correct Choice ... Try Again " << "\n";
-			}
-		}
-		catch (string e)
-		{
-			cout << e << "\n";
-		}
+        cin >> c;
 
-	}
-	return 0;
+        try
+        {
+            switch (c)
+            {
+            case 1:
+                cout << "Enter the element to be inserted: ";
+                cin >> ele;
+                obj.Insert(ele);
+                break;
+
+            case 2:
+                cout << "Top element is: " << obj.Poll() << "\n";
+                break;
+
+            case 3:
+                cout << "Enter the element to be deleted: ";
+                cin >> ele;
+                obj.Delete(ele);
+                obj.Display();
+                break;
+
+            case 4:
+                obj.Display();
+                break;
+
+            case 5:
+                exit(0);
+
+            default:
+                cout << "Enter Correct Choice... Try Again" << "\n";
+            }
+        }
+        catch (string e)
+        {
+            cout << e << "\n";
+        }
+    }
+
+    return 0;
 }
 

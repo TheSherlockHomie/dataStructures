ff4b28407d22db5589eba10f028bf1edbbbf7a45
Kushagra Kalash <kushagrakdev@gmail.com>
2020-10-24 11:51:12 +0530

    Issue with current implementation

--- LinkedList/SampleProgram.cpp
+++ LinkedList/SampleProgram.cpp
@@ -13,3 +13,13 @@ int main()
     auto n2 = std::make_unique<ds::Node>(2);
     n2->next = std::move(n1);
 }
+
+/*
+* Output:
+* Destroyed node with data: 2
+* Destroyed node with data: 1
+* Destroyed node with data: 0
+* 
+* Recursively deletes all Nodes.
+* Could causde stack overflow if size of list is too large
+*/
